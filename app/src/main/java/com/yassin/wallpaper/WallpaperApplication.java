fc16f1b82c7dea84a0413f1128383155b58d5edd
yassin <yassin123441@gmail.com>
2021-02-16 13:44:08 +0100

    Code style

--- app/src/main/java/com/yassin/wallpaper/WallpaperApplication.java
+++ app/src/main/java/com/yassin/wallpaper/WallpaperApplication.java
@@ -1,8 +1,12 @@
 package com.yassin.wallpaper;
 
 import android.app.Application;
+
 import dagger.hilt.android.HiltAndroidApp;
 
-/** Created by yassin baccour on 15/05/2016. */
+/**
+ * Created by yassin baccour on 15/05/2016.
+ */
 @HiltAndroidApp
-public class WallpaperApplication extends Application {}
+public class WallpaperApplication extends Application {
+}
