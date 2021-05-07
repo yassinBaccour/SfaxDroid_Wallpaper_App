fd5b4c84876f78e2b49c0c23932c430686e1cc66
yassin <yassin123441@gmail.com>
2021-05-07 14:12:51 +0200

    Fix Spotless

--- base/src/main/java/com/sfaxdroid/bases/Constants.kt
+++ base/src/main/java/com/sfaxdroid/bases/Constants.kt
@@ -2,7 +2,7 @@ package com.sfaxdroid.bases
 
 class Constants {
     companion object {
-        const val CURRENT_WALLPAPER_KEY = "CURRENT_WALLPAPER_KEY"
-        const val WALLPAPER_COLOR = "WALLPAPER_COLOR"
+        const val CURRENT_WALLPAPER_KEY = "CurrentWallpaperKey"
+        const val WALLPAPER_COLOR = "WallpaperColor"
     }
 }
