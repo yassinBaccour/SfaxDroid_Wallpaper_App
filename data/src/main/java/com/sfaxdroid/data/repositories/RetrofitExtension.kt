9b635f6280ea7c04a06e834e64c96d24ef0fdf2c
yassin <yassin123441@gmail.com>
2021-06-01 16:16:54 +0200

    Throw network error

--- data/src/main/java/com/sfaxdroid/data/repositories/RetrofitExtension.kt
+++ data/src/main/java/com/sfaxdroid/data/repositories/RetrofitExtension.kt
@@ -5,20 +5,29 @@ import com.sfaxdroid.data.entity.Response
 import retrofit2.Call
 import retrofit2.HttpException
 
-fun <T> Call<T>.executeWs(): retrofit2.Response<T> {
-    val call = if (isExecuted) clone() else this
-    return call.execute()
+fun <T> Call<T>.executeWs(): retrofit2.Response<T>? {
+    return try {
+        val call = if (isExecuted) clone() else this
+        call.execute()
+    } catch (e: Exception) {
+        null
+    }
 }
 
-fun <T> retrofit2.Response<T>.toResult(logger: Logger): Response<T> = try {
-    logger.d(raw().networkResponse()?.request()?.url().toString())
-    logger.d(message())
-    if (isSuccessful) {
-        Response.SUCCESS(body()!!)
-    } else
+fun <T> retrofit2.Response<T>?.toResult(logger: Logger): Response<T> {
+    if (this == null) return Response.FAILURE(Exception())
+    return try {
+        logger.d(raw().networkResponse()?.request()?.url().toString())
+        logger.d(message())
+        if (isSuccessful) {
+            Response.SUCCESS(body()!!)
+        } else
+            Response.FAILURE(toException())
+
+    } catch (e: Exception) {
         Response.FAILURE(toException())
-} catch (e: Exception) {
-    Response.FAILURE(toException())
+    }
 }
+
 
 fun <T> retrofit2.Response<T>.toException() = HttpException(this)
