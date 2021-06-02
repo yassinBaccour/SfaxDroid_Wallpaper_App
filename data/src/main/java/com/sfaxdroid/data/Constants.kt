159e0b6514669753fba52b9d1be0c648f0157ba2
yassin <yassin123441@gmail.com>
2021-06-02 10:08:42 +0200

    Remove mini folder

--- data/src/main/java/com/sfaxdroid/data/Constants.kt
+++ data/src/main/java/com/sfaxdroid/data/Constants.kt
@@ -1,6 +1,6 @@
 package com.sfaxdroid.data
 
 object Constants {
-    const val URL_BIG_WALLPAPER_FOLDER = "/islamicimages/"
-    const val URL_SMALL_WALLPAPER_FOLDER = "/islamicimagesmini/"
+    const val URL_BIG_WALLPAPER_FOLDER = "/ /"
+    const val URL_SMALL_WALLPAPER_FOLDER = "/ /"
 }
