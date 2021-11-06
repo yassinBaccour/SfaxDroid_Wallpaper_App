380e1694d7cc08ca02139197eb5fe74ec95d511f
yassin <yassin123441@gmail.com>
2021-11-06 18:45:43 +0100

    - Add JetPack Compose to mini-app-raining-anim - Migration mini-app-raining-anim UI to compose

--- mini-app-raining-anim/src/main/java/com/yassin/sfax/tawakkolalaallah/MainActivity.kt
+++ mini-app-raining-anim/src/main/java/com/yassin/sfax/tawakkolalaallah/MainActivity.kt
@@ -1,24 +1,99 @@
 package com.yassin.sfax.tawakkolalaallah
 
-import android.app.Activity
+import android.icu.number.Scale
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sfaxdroid.mini.base.Utils
 
-class MainActivity : Activity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.buttonSetWallpaper).setOnClickListener {
-            Utils.openLiveWallpaper<LiveWallpaper>(this)
-        }
-        findViewById<View>(R.id.buttonRating).setOnClickListener {
-            Utils.ratingApplication(this)
-        }
-        findViewById<View>(R.id.imageViewPub).setOnClickListener {
-            Utils.openPub(this)
+        setContent {
+            MyScreen()
         }
     }
+
+    @Composable
+    private fun MyScreen() {
+        val context = LocalContext.current
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxSize(),
+                painter = painterResource(R.drawable.wallpaper2),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                alignment = Alignment.TopCenter
+            )
+            Column(
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(),
+            ) {
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    onClick = { Utils.openLiveWallpaper<LiveWallpaper>(context) }) {
+                    Text(
+                        text = stringResource(id = R.string.set_wallpaper_click_text),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp,
+                    )
+                }
+                Button(modifier = Modifier.padding(10.dp),
+                    onClick = { Utils.ratingApplication(context) }) {
+                    Text(
+                        text = stringResource(id = R.string.setting_rate_us),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp,
+                    )
+                }
+                Spacer(
+                    Modifier
+                        .height(24.dp)
+                )
+                val img = painterResource(id = R.drawable.ic_pub)
+                Image(painter = img, contentDescription = "", modifier = Modifier.clickable(
+                    enabled = true,
+                    onClickLabel = "Clickable image",
+                    onClick = {
+                        Utils.openPub(context)
+                    }
+                ))
+            }
+        }
+
+    }
+
+    @Preview
+    @Composable
+    private fun preview() {
+        MyScreen()
+    }
+
 }
