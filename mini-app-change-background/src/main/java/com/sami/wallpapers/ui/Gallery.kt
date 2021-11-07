10b00e0b7f1c696596e78681e0b9db5e06944c4d
yassin <yassin123441@gmail.com>
2021-11-08 00:30:29 +0100

    Add Gallery

--- mini-app-change-background/src/main/java/com/sami/wallpapers/ui/Gallery.kt
+++ mini-app-change-background/src/main/java/com/sami/wallpapers/ui/Gallery.kt
@@ -1,9 +1,11 @@
 package com.sami.wallpapers.ui
 
 import android.app.WallpaperManager
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -11,14 +13,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.sami.wallpapers.Constants
-import com.sami.wallpapers.R
 import java.util.*
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Gallery() {
-    val iterator = (1..50).iterator()
+    val iterator = (1..98).iterator()
     val context = LocalContext.current
-    LazyRow(modifier = Modifier.fillMaxHeight()) {
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(minSize = 128.dp)
+    ) {
         iterator.forEach {
             val drawableRes = context.resources.getIdentifier(
                 Constants.RESOURCE_PREFIX + String.format("%05d", it, Locale.US), "drawable",
@@ -26,12 +30,12 @@ fun Gallery() {
             )
             item {
                 Image(
-                    modifier = Modifier
-                        .width(400.dp)
-                        .fillMaxHeight(),
+                    modifier = Modifier.clickable {
+                        //TODO navigate to detail Screen
+                    },
                     painter = painterResource(drawableRes),
                     contentDescription = null,
-                    contentScale = ContentScale.FillBounds
+                    contentScale = ContentScale.FillBounds,
                 )
             }
         }
