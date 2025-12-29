e53a185651b6962f6eca68d5d027e471b9c9b349
Yassin <yassin.baccour@externe.francemm.com>
2025-12-29 15:20:39 +0100

    Fix Tag Screen

--- ui/wallpapers/src/main/java/com/sfaxdroid/wallpapers/core/view/WallpaperGrid.kt
+++ ui/wallpapers/src/main/java/com/sfaxdroid/wallpapers/core/view/WallpaperGrid.kt
@@ -2,6 +2,7 @@ package com.sfaxdroid.wallpapers.core.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -13,6 +14,7 @@ internal fun WallpaperGrid(
     openDetail: (String) -> Unit
 ) {
     FlowRow(
+        modifier = Modifier.padding(horizontal = 4.dp),
         maxItemsInEachRow = 3,
         verticalArrangement = Arrangement.spacedBy(4.dp),
         horizontalArrangement = Arrangement.spacedBy(4.dp)
